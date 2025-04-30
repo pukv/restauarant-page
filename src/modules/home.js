935510d1e6cc8540090bf89f6b510aedbe55a61c
vukp <pukvavlovic10@gmail.com>
2025-05-01 01:47:05 +0200

    created an initial loadPage function, turned it into an IIFE so it loads the content right away, and added a title and some text about the restauraunt. Also added a css class to the body so it covers the entire page.

--- src/modules/home.js
+++ src/modules/home.js
@@ -1,11 +1,14 @@
 export default (function loadPage() {
   const content = document.querySelector("#content");
   const mainTitle = document.createElement("h1");
-  mainTitle.textContent = "Do you want us to tell you a secret?";
+  mainTitle.textContent = "Can you keep a secret?";
   content.append(mainTitle);
 })();
 
 const hypeText = document.createElement("h2");
-hypeText.textContent =
-  "Warning: Our food actually looks like the photos. The Republic's dishes are camera-ready and flavor-packed – no deceptive foodography here!";
+hypeText.textContent = `Our food actually looks like the photos. 
+The Republic's dishes are camera-ready and flavor-packed – no deceptive 
+foodography here!`;
 content.append(hypeText);
+
+document.body.classList.add("bg-image");
