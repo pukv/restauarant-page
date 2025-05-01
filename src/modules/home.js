c6e6378751230c3e0be66b30dea8f78163121ab0
vukp <pukvavlovic10@gmail.com>
2025-05-02 00:36:42 +0200

    added an additional selector that will help with swapping the background image once the page is switched from the home page. In the JS file, I had to rename the selector name.

--- src/modules/home.js
+++ src/modules/home.js
@@ -1,14 +1,14 @@
-export default (function loadPage() {
+export default function loadHomePage() {
   const content = document.querySelector("#content");
   const mainTitle = document.createElement("h1");
   mainTitle.textContent = "Can you keep a secret?";
   content.append(mainTitle);
-})();
 
-const hypeText = document.createElement("h2");
-hypeText.textContent = `Our food actually looks like the photos. 
+  const hypeText = document.createElement("h2");
+  hypeText.textContent = `Our food actually looks like the photos. 
 The Republic's dishes are camera-ready and flavor-packed – no deceptive 
 foodography here!`;
-content.append(hypeText);
+  content.append(hypeText);
 
-document.body.classList.add("bg-image");
+  document.body.classList.add("bg-image-main");
+}
