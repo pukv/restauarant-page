9f08f076f2077652b2eb93af98e19f9cd2052715
vukp <pukvavlovic10@gmail.com>
2025-05-02 00:20:12 +0200

    renamed the function for it to be more descriptive, also added a function inside which initilizes the page, hence the name initPage. Inside the function, first the home page is loaded, after which there are query selectors for the buttons who got assigned their functions on click.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
-import loadPage from "./modules/home";
+import loadHomePage from "./modules/home";
+import { loadContactPage } from "./modules/contact";
+import { loadMenuPage } from "./modules/menu";
 import "./styles.css";
+
+function initPage() {
+  loadHomePage();
+
+  const content = document.querySelector("#content");
+  const homeBtn = document.querySelector(".home-btn");
+  const contactBtn = document.querySelector(".contact-btn");
+  const menuBtn = document.querySelector(".menu-btn");
+
+  homeBtn.addEventListener("click", loadHomePage);
+  menuBtn.addEventListener("click", loadMenuPage);
+  contactBtn.addEventListener("click", loadContactPage);
+}
+
+document.addEventListener("DOMContentLoaded", initPage);
